0e981ffd2e4db423e1fd22d1eb49ede67917c7a9
Heley Danylo <78755636+StoRmGC228@users.noreply.github.com>
2026-03-02 01:43:33 +0200

    feat: Create db context and innstall nugets

---
+++ PocketGrail.Infrastructure/PocketGrailDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketGrail.Infrastructure
+{
+    using Microsoft.EntityFrameworkCore;
+
+    internal sealed class PocketGrailDbContext : DbContext
+    {
+        public PocketGrailDbContext(DbContextOptions<PocketGrailDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
