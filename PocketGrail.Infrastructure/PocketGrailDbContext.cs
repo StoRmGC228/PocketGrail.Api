c9eac7c4ba46f538565f55c5ea5cc82b0ddeb855
Heley Danylo <78755636+StoRmGC228@users.noreply.github.com>
2026-03-02 17:47:54 +0200

    feat:add empty db context

--- PocketGrail.Infrastructure/PocketGrailDbContext.cs
+++ PocketGrail.Infrastructure/PocketGrailDbContext.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace PocketGrail.Infrastructure
+namespace PocketGrail.Infrastructure;
+
+using Microsoft.EntityFrameworkCore;
+
+internal sealed class PocketGrailDbContext : DbContext
 {
-    using Microsoft.EntityFrameworkCore;
-
-    internal sealed class PocketGrailDbContext : DbContext
+    public PocketGrailDbContext(DbContextOptions<PocketGrailDbContext> options) : base(options)
     {
-        public PocketGrailDbContext(DbContextOptions<PocketGrailDbContext> options) : base(options)
-        {
-        }
+    }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
     }
 }
